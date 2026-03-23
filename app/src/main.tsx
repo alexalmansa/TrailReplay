de3c411d3d8bb1f7b09c963d50f0af23851bb277
Alex Almansa <alexalmansa5@gmail.com>
2026-03-23 12:10:21 +1100

    Remove broken GA4 bridge

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AppErrorBoundary } from '@/components/app/AppErrorBoundary'
-import { startAnalytics } from '@/utils/analytics'
 import { startWebVitalsTracking } from '@/utils/performance'
 
-void startAnalytics();
 void startWebVitalsTracking();
 
 createRoot(document.getElementById('root')!).render(
